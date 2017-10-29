3405f116cb8b1390f9e68695bea1803b60eb1345
Aarshad Devani <aadevani@deloitte.com>
2017-10-29 20:17:26 +0530

    Intial Code Push

---
+++ NightAtTheMuseum/Assets/Scripts/movementScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    public GameObject[] spotPoints;
+    public GameObject spotLight;
+
+    private GameObject previousWaypoint;
+
+    
+    //private GameObject currentWaypoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void setCurrentWaypoint(GameObject currentObject)
+    {
+        if (previousWaypoint)
+        {
+            toggleCollider(previousWaypoint);
+
+        }
+        //currentWaypoint = currentObject;
+        toggleCollider(currentObject);
+        //toggleCollider(previousWaypoint);
+        previousWaypoint = currentObject;
+    }
+
+    private void toggleCollider(GameObject thisGameObject)
+    {
+        (thisGameObject.GetComponent<Collider>()).enabled = !((thisGameObject.GetComponent<Collider>()).enabled);
+    }
+
+    private void setColliderMode(GameObject thisGameObject, bool toggleValue)
+    {
+        (thisGameObject.GetComponent<Collider>()).enabled = toggleValue;
+    }
+
+    public void showSpotLight(int number)
+    {
+        spotLight.transform.LookAt(spotPoints[number].transform);
+    }
+}
---
+++ NightAtTheMuseum/Assets/Scripts/waypoint.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoint : MonoBehaviour {
+
+    [Header("Waypoint Options")]
+    public bool isAreaWaypoint = false;
+    public GameObject player;
+    public bool teleport;
+    public GameObject[] informationPanels;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        if (isAreaWaypoint)
+        {
+            foreach(GameObject informationPanel in informationPanels)
+            {
+                informationPanel.SetActive(false);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onWaypointClick()
+    {
+        if (teleport)
+        {
+            player.transform.position = gameObject.transform.position;
+        }
+        else
+        {
+            iTween.MoveTo(player, iTween.Hash(
+                "position", gameObject.transform.position,
+                "time", 0.2,
+                "easetype", "linear"
+                )
+            );
+        }
+        if (isAreaWaypoint)
+        {
+            foreach (GameObject informationPanel in informationPanels)
+            {
+                informationPanel.SetActive(true);
+            }
+        }
+    }
+}
